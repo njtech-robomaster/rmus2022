b549ec745891d8c7c844615275501bca6cfe222c
Xu Zhaoqin <ixuzhaoqin@gmail.com>
2022-03-26 20:59:12 +0800

    State Machine

---
+++ src/finite_state_machine/scripts/Robot.py
@@ -0,0 +1,104 @@
+
+from __future__ import print_function
+from re import X
+from typing_extensions import Self
+import rospy
+import actionlib
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from std_msgs.msg import String, Bool
+from apriltag_msgs.msg import ApriltagMarkerArray
+
+
+
+class Robot(object):
+    def __init__(self):
+        self.statePub = rospy.Publisher('state', String, queue_size=10)
+        self.graspls = rospy.Subscriber(
+            'grasp_success', Bool, self.graspCallback)
+        self.planels = rospy.Subscriber(
+            'plane_success', Bool, self.planeCallback)
+        self.observe = rospy.Subscriber(
+            '/markers', ApriltagMarkerArray, self.ObserveCallback)
+
+        self.isCatch = False
+        self.isPlane = False
+        self.isGetNum = False
+        self.isFinish = False
+
+        self.onObserve = False
+
+        self.aruco = [0, 0, 0]
+        self.arucoNum = 0
+        self.findoreCount = 0
+
+        self.Rate = rospy.Rate(1)
+
+    def movebase(self, x, y, z, tx, ty, tz, tw):
+        client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
+        client.wait_for_server()
+
+        rospy.loginfo("Client")
+        goal = MoveBaseGoal()
+        goal.target_pose.header.frame_id = "map"
+        goal.target_pose.header.stamp = rospy.Time.now()
+        goal.target_pose.pose.position.x = x
+        goal.target_pose.pose.position.y = y
+        goal.target_pose.pose.position.z = z
+        goal.target_pose.pose.orientation.x = tx
+        goal.target_pose.pose.orientation.y = ty
+        goal.target_pose.pose.orientation.z = tz
+        goal.target_pose.pose.orientation.w = tw
+        rospy.loginfo("send_goal")
+        client.send_goal(goal)
+        wait = client.wait_for_result()
+        if not wait:
+            rospy.logerr("move_base server not available!")
+            rospy.signal_shutdown("Action server not available!")
+        else:
+            return client.get_result()
+
+    def move(self, **args):
+        result = self.movebase(**args)
+        try:
+            while not result:
+                pass
+        except rospy.ROSException:
+            raise TimeoutError
+
+    def graspCallback(self, data):
+        if data.data:
+            self.isCatch = True
+        else:
+            self.isCatch = False
+
+    def planeCallback(self, data):
+        if data.data:
+            self.isPlane = True
+        else:
+            self.isPlane = False
+
+    def ObserveCallback(self, data):
+        if not self.onObserve:
+            return
+        if len(data) == 1:
+            rospy.logwarn("Only see one tag")
+            return
+
+        self.arucoNum = 0
+        self.isGetNum = False
+
+        data = data.markers
+        arucoGet = []
+        for x in data:
+            if x.id in [0, 1, 2, 3, 4]:
+                arucoGet.append(x)
+        arucoGet = sorted(
+            arucoGet, key=lambda x: x.pose.position.z, reverse=False)
+        for x in range(0, min(3, len(arucoGet))):
+            self.aruco[x] = arucoGet[x].id+1
+            self.arucoNum += 1
+            self.isGetNum = True
+
+    def exitObserve(self):
+        self.onObserve = False
+
