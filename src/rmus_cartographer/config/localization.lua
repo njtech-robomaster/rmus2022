a9be36ee580c100da800fdcf8c2bafc09f3d79b9
Haowei Wen <yushijinhun@gmail.com>
2022-05-12 07:47:00 +0800

    add urdf & cartographer nav

---
+++ src/rmus_cartographer/config/localization.lua
@@ -0,0 +1,7 @@
+include "mapping.lua"
+
+TRAJECTORY_BUILDER.pure_localization_trimmer = {
+    max_submaps_to_keep = 3,
+}
+
+return options
---
+++ src/rmus_cartographer/scripts/write_map.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+
+map_dir=$(realpath "$1")
+mkdir -p "$map_dir"
+
+rosservice call /finish_trajectory 0
+rosservice call /write_state "filename: '$map_dir/map.pbstream'"
+rosrun cartographer_ros cartographer_pbstream_to_ros_map \
+    -map_filestem="$map_dir/map" \
+    -pbstream_filename="$map_dir/map.pbstream" \
+    -resolution=0.05 \
+    -initial_pose '{to_trajectory_id = 0, relative_pose = { translation = { 0., 0., 0. }, rotation = { 0.0, 0.0, 0.0 } } }'
