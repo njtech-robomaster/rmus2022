2fdf78775ca970f565ebf95a18cf23768d900b26
Haowei Wen <yushijinhun@gmail.com>
2022-05-20 09:08:36 +0000

    remove use of cmd_position

--- src/arm_controller/src/chassis_move.cpp
+++ src/arm_controller/src/chassis_move.cpp
@@ -3,23 +3,15 @@
 #include <nav_msgs/Odometry.h>
 #include <tuple>
 
-constexpr double STATIC_LINEAR_VELOCITY = 0.01;
-constexpr double STATIC_ANGULAR_VELOCITY = 0.02;
-constexpr double IDLE_TIME = 0.3;
-constexpr double X_TOLERANCE = 0.15;
-constexpr double Y_TOLERANCE = 0.15;
-constexpr double THETA_TOLERANCE = 0.15;
-constexpr int MAX_RETRY_COUNT = 5;
-constexpr double X_TOLERANCE2 = 0.01;
-constexpr double Y_TOLERANCE2 = 0.01;
-constexpr double YAW_TOLERANCE2 = 0.01;
+static constexpr double X_TOLERANCE = 0.01;
+static constexpr double Y_TOLERANCE = 0.01;
+static constexpr double YAW_TOLERANCE = 0.01;
 
-constexpr double sign(double x) {
+static constexpr double sign(double x) {
 	return x < 0 ? -1 : 1;
 }
 
 ChassisMove::ChassisMove() {
-	postion_pub = nh.advertise<geometry_msgs::Twist>("/cmd_position", 1);
 	chassis_move_target_pub =
 	    nh.advertise<geometry_msgs::PoseStamped>("chassis_move_target", 1);
 	cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/cmd_vel", 1);
@@ -31,151 +23,54 @@ ChassisMove::ChassisMove() {
 		    double odom_x = odom->pose.pose.position.x;
 		    double odom_y = odom->pose.pose.position.y;
 		    double odom_yaw = get_yaw(odom->pose.pose.orientation);
-		    double odom_vtheta = odom->twist.twist.angular.z;
 
-		    double odom_vx, odom_vy;
-		    if (last_odom_pos.has_value()) {
-			    double dt =
-			        (odom->header.stamp - std::get<0>(*last_odom_pos)).toSec();
-			    odom_vx = (odom_x - std::get<1>(*last_odom_pos)) / dt;
-			    odom_vy = (odom_y - std::get<2>(*last_odom_pos)) / dt;
-		    } else {
-			    odom_vx = 0;
-			    odom_vy = 0;
-		    }
-		    last_odom_pos = {odom->header.stamp, odom_x, odom_y};
-
-		    auto send_cmd_position = [&] {
-			    geometry_msgs::Twist pos_cmd;
-
-			    if (std::abs(goal.error_theta) > THETA_TOLERANCE) {
-				    pos_cmd.angular.z = goal.error_theta;
-				    ROS_INFO("Adjusting yaw");
-			    } else {
-				    pos_cmd.linear.x = goal.error_x;
-				    pos_cmd.linear.y = goal.error_y;
-				    ROS_INFO("Adjusting x,y");
-			    }
-
-			    postion_pub.publish(pos_cmd);
-		    };
-
-		    switch (goal.state) {
-
-		    case MoveFeedback::State::INIT: {
-			    goal.state = MoveFeedback::State::MOVING;
+		    if (goal.state == MoveFeedback::State::INIT) {
 			    goal.goal_x = odom_x + goal.goal_dx * std::cos(odom_yaw) -
 			                  goal.goal_dy * std::sin(odom_yaw);
 			    goal.goal_y = odom_y + goal.goal_dx * std::sin(odom_yaw) +
 			                  goal.goal_dy * std::cos(odom_yaw);
 			    goal.goal_theta = normalize_angle(odom_yaw + goal.goal_dtheta);
 
-			    double ex = goal.goal_x - odom_x;
-			    double ey = goal.goal_y - odom_y;
-			    goal.error_x =
-			        ex * std::cos(odom_yaw) + ey * std::sin(odom_yaw);
-			    goal.error_y =
-			        -ex * std::sin(odom_yaw) + ey * std::cos(odom_yaw);
-			    goal.error_theta = normalize_angle(goal.goal_theta - odom_yaw);
-			    goal.idle_since = ros::Time::now();
+			    goal.state = MoveFeedback::State::FIXING_YAW;
+		    }
 
-			    geometry_msgs::PoseStamped debug_msg;
-			    debug_msg.header.frame_id = "base_link";
-			    debug_msg.header.stamp = odom->header.stamp;
-			    debug_msg.pose.position.x = goal.goal_dx;
-			    debug_msg.pose.position.y = goal.goal_dy;
-			    debug_msg.pose.orientation =
-			        quaternionMsgFromYaw(goal.goal_dtheta);
-			    chassis_move_target_pub.publish(debug_msg);
+		    double ex = goal.goal_x - odom_x;
+		    double ey = goal.goal_y - odom_y;
+		    goal.error_x = ex * std::cos(odom_yaw) + ey * std::sin(odom_yaw);
+		    goal.error_y = -ex * std::sin(odom_yaw) + ey * std::cos(odom_yaw);
+		    goal.error_theta = normalize_angle(goal.goal_theta - odom_yaw);
 
-			    ROS_INFO_STREAM("Goal dx=" << goal.error_x
-			                               << " dy=" << goal.error_y
-			                               << " dtheta=" << goal.error_theta);
+		    switch (goal.state) {
 
-			    send_cmd_position();
+		    case MoveFeedback::State::FIXING_YAW: {
 
-			    current_callback(goal);
-
-		    } break;
-
-		    case MoveFeedback::State::MOVING: {
-
-			    double ex = goal.goal_x - odom_x;
-			    double ey = goal.goal_y - odom_y;
-			    goal.error_x =
-			        ex * std::cos(odom_yaw) + ey * std::sin(odom_yaw);
-			    goal.error_y =
-			        -ex * std::sin(odom_yaw) + ey * std::cos(odom_yaw);
-			    goal.error_theta = normalize_angle(goal.goal_theta - odom_yaw);
-
-			    double linear_vel =
-			        std::sqrt(odom_vx * odom_vx + odom_vy * odom_vy);
-			    double angular_vel = std::abs(odom_vtheta);
-			    if (linear_vel > STATIC_LINEAR_VELOCITY ||
-			        angular_vel > STATIC_ANGULAR_VELOCITY) {
-				    goal.idle_since = ros::Time::now();
+			    bool moving = false;
+			    geometry_msgs::Twist twist;
+			    if (std::abs(goal.error_theta) > YAW_TOLERANCE) {
+				    twist.angular.z = 0.1 * sign(goal.error_theta);
+				    moving = true;
 			    }
-
-			    if ((ros::Time::now() - goal.idle_since).toSec() > IDLE_TIME) {
-
-				    if (std::abs(goal.error_x) > X_TOLERANCE ||
-				        std::abs(goal.error_y) > Y_TOLERANCE ||
-				        std::abs(goal.error_theta) > THETA_TOLERANCE) {
-
-					    if (goal.retry_count >= MAX_RETRY_COUNT) {
-						    ROS_INFO_STREAM("Goal failed dx="
-						                    << goal.error_x
-						                    << " dy=" << goal.error_y
-						                    << " dtheta=" << goal.error_theta);
-						    goal.state = MoveFeedback::State::FAIL;
-						    auto cb = current_callback;
-						    current_callback = nullptr;
-						    cb(goal);
-
-					    } else {
-						    goal.idle_since = ros::Time::now();
-						    goal.retry_count++;
-
-						    ROS_INFO_STREAM("Fix goal dx="
-						                    << goal.error_x
-						                    << " dy=" << goal.error_y
-						                    << " dtheta=" << goal.error_theta);
-
-						    send_cmd_position();
-
-						    current_callback(goal);
-					    }
-
-				    } else {
-
-					    ROS_INFO_STREAM("Goal moving done dx="
-					                    << goal.error_x
-					                    << " dy=" << goal.error_y
-					                    << " dtheta=" << goal.error_theta);
-					    goal.state = MoveFeedback::State::FIXING_YAW;
-					    current_callback(goal);
-				    }
+			    cmd_vel_pub.publish(twist);
+			    if (moving) {
+				    current_callback(goal);
+			    } else {
+				    ROS_INFO_STREAM("Goal fixing_xy done dx="
+				                    << goal.error_x << " dy=" << goal.error_y
+				                    << " dtheta=" << goal.error_theta);
+				    goal.state = MoveFeedback::State::FIXING_XY;
+				    current_callback(goal);
 			    }
-
 		    } break;
 
 		    case MoveFeedback::State::FIXING_XY: {
 
-			    double ex = goal.goal_x - odom_x;
-			    double ey = goal.goal_y - odom_y;
-			    goal.error_x =
-			        ex * std::cos(odom_yaw) + ey * std::sin(odom_yaw);
-			    goal.error_y =
-			        -ex * std::sin(odom_yaw) + ey * std::cos(odom_yaw);
-			    goal.error_theta = normalize_angle(goal.goal_theta - odom_yaw);
-
 			    bool moving = false;
 			    geometry_msgs::Twist twist;
-			    if (std::abs(goal.error_x) > X_TOLERANCE2) {
+			    if (std::abs(goal.error_x) > X_TOLERANCE) {
 				    twist.linear.x = 0.1 * sign(goal.error_x);
 				    moving = true;
 			    }
-			    if (std::abs(goal.error_y) > Y_TOLERANCE2) {
+			    if (std::abs(goal.error_y) > Y_TOLERANCE) {
 				    twist.linear.y = 0.1 * sign(goal.error_y);
 				    moving = true;
 			    }
@@ -192,34 +87,6 @@ ChassisMove::ChassisMove() {
 				    cb(goal);
 			    }
 
-		    } break;
-
-		    case MoveFeedback::State::FIXING_YAW: {
-
-			    double ex = goal.goal_x - odom_x;
-			    double ey = goal.goal_y - odom_y;
-			    goal.error_x =
-			        ex * std::cos(odom_yaw) + ey * std::sin(odom_yaw);
-			    goal.error_y =
-			        -ex * std::sin(odom_yaw) + ey * std::cos(odom_yaw);
-			    goal.error_theta = normalize_angle(goal.goal_theta - odom_yaw);
-
-			    bool moving = false;
-			    geometry_msgs::Twist twist;
-			    if (std::abs(goal.error_theta) > YAW_TOLERANCE2) {
-				    twist.angular.z = 0.1 * sign(goal.error_theta);
-				    moving = true;
-			    }
-			    cmd_vel_pub.publish(twist);
-			    if (moving) {
-				    current_callback(goal);
-			    } else {
-				    ROS_INFO_STREAM("Goal fixing_xy done dx="
-				                    << goal.error_x << " dy=" << goal.error_y
-				                    << " dtheta=" << goal.error_theta);
-				    goal.state = MoveFeedback::State::FIXING_XY;
-				    current_callback(goal);
-			    }
 		    } break;
 
 		    default:
@@ -239,7 +106,6 @@ void ChassisMove::execute(double dx, double dy, double dtheta,
 	}
 
 	current_callback = callback;
-	last_odom_pos = std::nullopt;
 	goal = MoveFeedback();
 	goal.state = MoveFeedback::State::INIT;
 	goal.goal_dx = dx;
